c5bddd28d36242f42261aaf719796d0de5059193
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-11 16:59:37 -0700

    fix

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+print(('object-' 'oriented ' 'programming ' 'with ' 'Python\n')[1:-1])
