81e66f4a9811a3561d0161e878ebc1fde1feea01
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-12 07:25:19 -0700

    'fix

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+
+last_digit = abs(number) % 10
+
+
+if last_digit > 5:
+    message = "and is greater than 5"
+elif last_digit == 0:
+    message = "and is 0"
+else:
+    message = "and is less than 6 and not 0"
+
+print("The string Last digit of", number, "is", last_digit, message)
