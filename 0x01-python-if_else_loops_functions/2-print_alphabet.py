1050c7d07636b97bcb4039655431c5d115ee1e3b
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-12 07:58:38 -0700

    'fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-import random
+# 2-print_alphabet.p
 
-number = random.randint(-10000, 10000)
-
-last_digit = abs(number) % 10
-comparison = ""
-
-if last_digit > 5:
-    comparison = "and is greater than 5"
-elif last_digit == 0:
-    comparison = "and is 0"
-else:
-    comparison = "and is less than 6 and not 0"
-
-print(f"The string Last digit of {number} is {last_digit} {comparison}\n")
+for i in range(97, 123):
+    print(chr(i), end='')
