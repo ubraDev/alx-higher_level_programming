c7676d2937d0d5526d42714c228b7e9a0341c4f4
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-14 08:15:17 -0700

    fx

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
-    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+
+    result = (a[0] + b[0], a[1] + b[1])
+    return (result)
