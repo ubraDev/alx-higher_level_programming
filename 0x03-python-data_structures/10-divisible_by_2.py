0d72d79c7cd71071d85c6eb8e05ca8aafe887bc7
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-14 08:47:57 -0700

    fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    boolist = my_list[:]
-    for count, i in enumerate(my_list):
-        if i % 2 == 0:
-            boolist[count] = True
-        else:
-            boolist[count] = False
-    return(boolist)
+    result = []
+
+    for num in my_list:
+        result.append(num % 2 == 0)
+
+    return (result)
