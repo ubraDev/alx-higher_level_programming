78c0ad4fba9acce46c23a91fcdbd18951fb39467
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-14 05:50:33 -0700

    fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,11 @@
-.4-new_in_list.py.swp
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+if idx < 0 or idx >= len(my_list):
+return my_list.copy()  # Return a copy of the original list
+        
+
+new_list = my_list.copy()
+
+new_list[idx] = element
+
+return new_list
