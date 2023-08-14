a254f8d756bf59bbcaeee2ba994c2893b79ead91
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-14 10:12:34 -0700

    fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
+
+def max_integer(my_list):
     if len(my_list) == 0:
-        return ("None")
-    x = my_list[0]
-    for i in my_list:
-        if i > x:
-            x = i
-    return (x)
+        return (None)
+
+    max_num = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+
+    return (max_num)
