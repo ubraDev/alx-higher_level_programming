01fd92451a6cf9e91aa82dcd226bc0d71cb1ed7e
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-16 10:43:18 -0700

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    squared_matrix = []
 
-    for row in matrix:
-        squared_row = []
-        for value in row:
-            squared_row.append(value ** 2)
-        squared_matrix.append(squared_row)
-    return (squared_matrix)
+def square_matrix_simple(matrix=[]):
+    new_matrix = [row[:] for row in matrix]
+    for idx, row in enumerate(new_matrix):
+        for idx2, col in enumerate(new_matrix):
+            new_matrix[idx][idx2] = row[idx2] ** 2
+    return (new_matrix)
