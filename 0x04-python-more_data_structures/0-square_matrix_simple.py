edc7724ff179cd2c7d2f7a6e46616275904b3f68
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-16 03:45:44 -0700

    add

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+
+    for row in matrix:
+        squared_row = []
+        for value in row:
+            squared_row.append(value ** 2)
+        squared_matrix.append(squared_row)
+    return (squared_matrix)
