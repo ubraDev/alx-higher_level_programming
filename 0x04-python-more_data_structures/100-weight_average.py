202ff19fb586024a8acc087a9b49b42e4bd0cb0c
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-16 14:34:43 -0700

    fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
-        return (0)
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
 
-    roman_numerals = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
+    num = 0
+    den = 0
 
-    prev_value = 0
-    total_value = 0
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
 
-    for char in reversed(roman_string):
-        value = roman_numerals.get(char, 0)
-
-        if value >= prev_value:
-            total_value += value
-        else:
-            total_value -= value
-
-        prev_value = value
-
-    return (total_value)
+    return (num / den)
