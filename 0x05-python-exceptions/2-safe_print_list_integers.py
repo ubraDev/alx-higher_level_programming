36a90232e7e94bf32bbabe40368af4d8639b9558
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-28 08:46:13 -0700

    add

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for item in my_list:
+            if isinstance(item, int):
+                print("{:d}".format(item), end=" ")
+                count += 1
+            if count >= x:
+                break
+        print()
+        return count
+    except:
+        return count
