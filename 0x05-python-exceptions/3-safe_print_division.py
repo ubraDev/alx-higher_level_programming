80d462e7e6abcd92d2d1a24dde50d4e5d23e3a39
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-28 12:30:03 -0700

    fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+
 def safe_print_division(a, b):
     try:
-        result = a / b
-    except ZeroDivisionError:
-        return None
+        div = a / b  # Attempt to perform the division
+    except (TypeError, ZeroDivisionError):  # Handle division by zero or type error
+        div = None  # Set div to None in case of error
     finally:
-        try:
-            print("Inside result: {}".format(result))
-        except NameError:
-            pass
-    return result
+        print("Inside result: {}".format(div))  # Always print the result, regardless of error
+    return div  # Return the division result or None
+
+# Test cases
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
