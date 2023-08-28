a7aa3865cfc1d9d3fb95febaae93f0c74f50e2f1
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-28 12:37:54 -0700

    fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result_list = []
+  """
+  Divides element by element 2 lists.
 
-    try:
-        for i in range(list_length):
-            try:
-                if i >= len(my_list_1) or i >= len(my_list_2):
-                    raise IndexError("out of range")
-                
-                element_1 = my_list_1[i]
-                element_2 = my_list_2[i]
-                
-                if not (isinstance(element_1, (int, float)) and isinstance(element_2, (int, float))):
-                    print("wrong type")
-                    result_list.append(0)
-                    continue
-                
-                if element_2 == 0:
-                    print("division by 0")
-                    result_list.append(0)
-                else:
-                    result_list.append(element_1 / element_2)
-            except IndexError as e:
-                print(e)
-                result_list.append(0)
-    except:
-        pass
-    
-    return result_list
+  Args:
+    my_list_1: The first list.
+    my_list_2: The second list.
+    list_length: The length of the new list.
+
+  Returns:
+    A new list (length = list_length) with all divisions.
+  """
+
+  new_list = []
+  try:
+    for i in range(list_length):
+      if not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
+        print("wrong type")
+      elif my_list_2[i] == 0:
+        print("division by 0")
+      else:
+        new_list.append(my_list_1[i] / my_list_2[i])
+  except IndexError:
+    print("out of range")
+  finally:
+    return new_list
