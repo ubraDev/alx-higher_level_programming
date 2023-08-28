ce47b21218d792c3171f93692882dc24c6dc6369
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-28 13:21:36 -0700

    fix

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-safe_print_list_integers = \
-            __import__('2-safe_print_list_integers').safe_print_list_integers
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return result
