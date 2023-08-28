5ae2fc36e3ad3e7a34d5c903ee4dbbf7d105efb4
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-28 12:03:58 -0700

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        count = 0
-        while count is not x:
-                print(my_list[count], end='')
-                count += 1
+        while i is not x:
+                print(my_list[i], end='')
+                i += 1
 
     except IndexError:
             None
         print()
-        return count
+        return i
