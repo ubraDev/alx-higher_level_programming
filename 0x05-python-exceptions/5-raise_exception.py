c57d05ace44d3e49f2041f23cad2193fd4e9637a
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-28 12:44:10 -0700

    fix

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,30 +1,6 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
-    result_list = []
 
-    try:
-        for i in range(list_length):
-            try:
-                if i >= len(my_list_1) or i >= len(my_list_2):
-                    raise IndexError("out of range")
 
-                element_1 = my_list_1[i]
-                element_2 = my_list_2[i]
-
-                if not (isinstance(element_1, (int, float)) and isinstance(element_2, (int, float))):
-                    print("wrong type")
-                    result_list.append(0)
-                    continue
-
-                if element_2 == 0:
-                    print("division by 0")
-                    result_list.append(0)
-                else:
-                    result_list.append(element_1 / element_2)
-            except IndexError as e:
-                print(e)
-                result_list.append(0)
-    except:
-        pass
-
-    return result_list
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def raise_exception_msg(message=""):
-    raise NameError(message)
 
-try:
-    raise_exception_msg("This is a custom name exception.")
-except NameError as e:
-    print("Exception:", e)
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
