12ba1c37cb9e92e8f752ff50199887125d3912d4
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-28 08:28:50 -0700

    add

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for item in my_list:
+            if count < x:
+                print(item, end=" ")
+                count += 1
+            else:
+                break
+        print()
+        return count
+    except:
+        return count
