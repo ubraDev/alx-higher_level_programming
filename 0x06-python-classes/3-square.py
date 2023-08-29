11dd69e7ef677e2a7814a045c1314f7599119912
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-29 11:47:22 -0700

    add

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
+"""Square class defination."""
+
 
 class Square:
-    """A class representing a square."""
+    """Square class body."""
 
     def __init__(self, size=0):
-        """
-        Initialize a Square instance.
-        
+        """Square contructor.
         Args:
-            size (int): The size of the square side (default is 0).
-        
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
-        """
-        Calculate the area of the square.
-        
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size * self.__size
+        """Return the new area of the square."""
+        return (self.__size * self.__size)
