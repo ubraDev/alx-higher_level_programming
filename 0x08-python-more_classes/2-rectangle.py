e5a96e7c01928a15dddbbf632af3e5c57eeaa7f0
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-09-06 13:09:10 -0700

    add

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+
+    def area(self):
+        return self._width * self._height
+
+    def perimeter(self):
+        return 2 * (self._width + self._height)
