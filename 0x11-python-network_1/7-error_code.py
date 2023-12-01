ed9610271ef6cc3539b4c5345ec710ab21446d32
brahim el alami chentoufi <bra.el.alami@gmail.com>
2023-12-01 12:53:26 -0800

    add

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
 """
-Python Script that takes in a URL, sends a request to the URL
-& displays the body of the response
-"""
+import sys
 import requests
-from sys import argv
 
-if __name__ == '__main__':
-    r = requests.get(argv[1])
-    status = r.status_code
-    print(r.text) if status < 400 else print(
-        "Error code: {}".format(r.status_code))
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
